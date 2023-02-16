97750289f5bfc037d7787aa930e7178896850025
Nifacia Bell <nifaciabell.tech@gmail.com>
2023-02-15 20:59:02 -0500

    Wild Wild Life answered

--- day2_total_recall.js
+++ day2_total_recall.js
@@ -1,20 +1,20 @@
 ////////////////////////////////
 //* Easy Going
 ////////////////////////////////
-// for (let i = 1; i <= 20; i++){
-//   console.log(i)
-// }
+for (let i = 1; i <= 20; i++){
+  console.log(i)
+}
 
 ////////////////////////////////
 //* Get Even
 ////////////////////////////////
-// for (let i = 0; i <=200; i++) {
+for (let i = 0; i <=200; i++) {
   
-//   if (i % 2 === 0) {
-//   console.log(i);
-//   } 
+  if (i % 2 === 0) {
+  console.log(i);
+  } 
  
-// }
+}
 
 ////////////////////////////////
 //* Fizz Buzz
@@ -32,13 +32,24 @@ if (i % 3) {
 }
 }
 
-
-
-
 ////////////////////////////////
 //* Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+
+wolfy[2] = 17
+console.log(wolfy[2])
+wolfy.splice(3, 0 , "Gotham City")
+console.log(wolfy[3])
+dart.push('Hawkins')
+console.log(dart)
+wolfy[0]= "Gameboy"
+console.log(wolfy)
 
 ////////////////////////////////
 //* Yell at the Ninja Turtles
